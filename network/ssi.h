3ff2d6229409d802c3f9a785ba14c5a15294a0e1
mtt <12588174+possibly-not@users.noreply.github.com>
2024-04-08 23:32:24 +0100

    Move things to debug section

--- network/ssi.h
+++ network/ssi.h
@@ -9,24 +9,29 @@
 
 uint32_t counter = 0;
 
-
 // SSI tags - tag length limited to 8 bytes by default
-const char * ssi_tags[] = {"counter", "dist"};
+const char *ssi_tags[] = {"counter", "dist", "heapinfo"};
 
-u16_t ssi_handler(int iIndex, char *pcInsert, int iInsertLen) {
+u16_t ssi_handler(int iIndex, char *pcInsert, int iInsertLen)
+{
   size_t printed;
-  switch (iIndex) {
+  switch (iIndex)
+  {
   case 0: // counter
-    {
-      printed = snprintf(pcInsert, iInsertLen, "%d", counter);
-    }
-    break;
+  {
+    printed = snprintf(pcInsert, iInsertLen, "%d", counter);
+  }
+  break;
   case 1: // counter distance
-    {
-      printed = snprintf(pcInsert, iInsertLen, "%.05fkm", (counter * WHEEL_SIZE_CM * PI) * 0.00001);
-    }
-    break;
-
+  {
+    printed = snprintf(pcInsert, iInsertLen, "%.05fkm", (counter * WHEEL_SIZE_CM * PI) * 0.00001);
+  }
+  break;
+  case 2: // heap info
+  {
+    printed = snprintf(pcInsert, iInsertLen, "%d out of %d", getFreeHeap(), getTotalHeap());
+  }
+  break;
   default:
     printed = 0; // bogos binted
     break;
@@ -35,10 +40,10 @@ u16_t ssi_handler(int iIndex, char *pcInsert, int iInsertLen) {
   return (u16_t)printed;
 }
 // Initialise the SSI handler
-void ssi_init(uint32_t *counter_ref) {
+void ssi_init(uint32_t *counter_ref)
+{
   // setup links (sorry global vars)
   counter = counter_ref;
-
 
   http_set_ssi_handler(ssi_handler, ssi_tags, LWIP_ARRAYSIZE(ssi_tags));
 }
